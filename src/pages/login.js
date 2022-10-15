65a193c053c1308660ee47b1ba844b44216363f1
Alperenkarslix <alperenkarsliceng@gmail.com>
2022-10-15 19:42:38 +0300

    yeni login

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,6 +8,7 @@ function Login() {
     console.log("login");
   };
   return (
+<<<<<<< HEAD
     <div className="main" >
       <div className="login">
         <h1>Log in!</h1>
@@ -18,6 +19,34 @@ function Login() {
         </form>
         </div>
     </div>
+=======
+    
+    <body>
+      <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet"></link>
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true"></input>
+
+			  <div class="signup">
+				  <form>
+					  <label for="chk" aria-hidden="true">Sign up</label>
+					  <input type="text" name="txt" placeholder="User name" required=""></input>
+					  <input type="email" name="email" placeholder="Email" required=""></input>
+					  <input type="password" name="pswd" placeholder="Password" required=""></input>
+					  <button>Sign up</button>
+				  </form>
+			  </div>
+
+			    <div class="login">
+				    <form>
+					  <label for="chk" aria-hidden="true">Login</label>
+					  <input type="email" name="email" placeholder="Email" required=""></input>
+					  <input type="password" name="pswd" placeholder="Password" required=""></input>
+					  <button>Login</button>
+			  	  </form>
+		    	</div>
+	    </div>
+      </body>
+>>>>>>> 2436bdc (yeni login)
     );
 }
 
