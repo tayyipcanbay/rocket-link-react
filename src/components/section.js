926a6d92e25b2da821f01d11280ea21bab16e124
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-15 18:41:52 +0300

    sync

--- src/components/section.js
+++ src/components/section.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "../style/section.css";
 
-function Section() {
+function Section({title,description}){
     return(
         <section>
             <div className="section">
                 <header>
-                    <h1>ROKET LINK</h1><hr></hr>
+                    <h1>{title}</h1><hr></hr>
                 </header>
                 <div className="content">
                     <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                        Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                        {description}
                     </p>
                 </div>
             </div>
