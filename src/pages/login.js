c98b6229b72f0a422bd4d7b32a548fae7a96d17f
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-15 19:41:43 +0300

    login

---
+++ src/pages/login.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../style/login.css';
+import {Link} from 'react-router-dom';
+
+
+function Login() {
+  const loginHandler = (e) => {
+    e.preventDefault();
+    console.log("login");
+  };
+  return (
+    <div className="main" >
+      <div className="login">
+        <h1>Log in!</h1>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button onClick={loginHandler} type="submit">Login</button>
+        </form>
+        </div>
+    </div>
+    );
+}
+
+export default Login;
