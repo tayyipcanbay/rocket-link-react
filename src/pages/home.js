403bac64dd9a14db8a777d3343b9a58c53bbb133
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-15 17:13:41 +0300

    section

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ function Home() {
     return (
         <div style={{backgroundColor:"black",width:"100vw",height:"100vh"}}>
             <Navbar />
-
+            
             <Footer />
         </div>
     )
