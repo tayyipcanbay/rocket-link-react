7403ae6d65ebba5fb734d24205a0e8e6e45110ff
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-15 18:03:09 +0300

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,29 +4,25 @@ import logo from"../assets/roket.png"
 
 function Navbar(){
     return(
-        <nav class="navbar">
-     <div class="logo">ROCKET LINK</div>
-     <ul class="nav-links">
-       <input type="checkbox" id="checkbox_toggle" />
-       <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-       <div class="menu">
-         <li><a href="/">Home</a></li>
-         <li><a href="/">About</a></li>
-         <li class="services">
-           <a href="/">Services</a>
-           <ul class="dropdown">
-             <li><a href="/">Dropdown 1 </a></li>
-             <li><a href="/">Dropdown 2</a></li>
-             <li><a href="/">Dropdown 2</a></li>
-             <li><a href="/">Dropdown 3</a></li>
-             <li><a href="/">Dropdown 4</a></li>
-           </ul>
-         </li>
-         <li><a href="/">Pricing</a></li>
-         <li><a href="/">Contact</a></li>
-       </div>
-     </ul>
-   </nav>
+        <nav>
+           <div className="navbar">
+                <div className="brand">
+                    <h1>ROKET LINK</h1>
+                </div>
+                <div className="menu">
+                    <div className="menu-item">
+                        <a href="#">Home</a>
+                    </div>    
+                    <div className="menu-item">
+                    <a href="#about">About</a>
+                    </div>    
+                    <div className="menu-item">
+                    <a href="#contact">Contact</a>
+                    </div>   
+                </div>
+           </div>
+        </nav>
+
     )
 }
 export default Navbar;
