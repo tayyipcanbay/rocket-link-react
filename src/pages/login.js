2436bdc2574e1c9b5feb7ef3f886c6b241deaf12
Alperenkarslix <alperenkarsliceng@gmail.com>
2022-10-15 19:42:38 +0300

    yeni login

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,16 +4,32 @@ import '../style/login.css';
 
 function Login() {
   return (
-    <div className="main" >
-      <div className="login">
-        <h1>Log in!</h1>
-        <form>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
-        </form>
-        </div>
-    </div>
+    
+    <body>
+      <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet"></link>
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true"></input>
+
+			  <div class="signup">
+				  <form>
+					  <label for="chk" aria-hidden="true">Sign up</label>
+					  <input type="text" name="txt" placeholder="User name" required=""></input>
+					  <input type="email" name="email" placeholder="Email" required=""></input>
+					  <input type="password" name="pswd" placeholder="Password" required=""></input>
+					  <button>Sign up</button>
+				  </form>
+			  </div>
+
+			    <div class="login">
+				    <form>
+					  <label for="chk" aria-hidden="true">Login</label>
+					  <input type="email" name="email" placeholder="Email" required=""></input>
+					  <input type="password" name="pswd" placeholder="Password" required=""></input>
+					  <button>Login</button>
+			  	  </form>
+		    	</div>
+	    </div>
+      </body>
     );
 }
 
