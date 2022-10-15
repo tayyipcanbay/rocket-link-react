ce512a84195ba346fa053a6d0ec3216067e1c62d
Alperenkarslix <alperenkarsliceng@gmail.com>
2022-10-15 20:47:25 +0300

    login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,32 +4,29 @@ import '../style/login.css';
 
 function Login() {
   return (
-    
-    <body>
-      <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet"></link>
-	<div class="main">  	
-		<input type="checkbox" id="chk" aria-hidden="true"></input>
+		<body>
+			<div class="main">  	
+				<input type="checkbox" id="chk" aria-hidden="true"></input>
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="txt" placeholder="User name" required=""></input>
+					<input type="email" name="email" placeholder="Email" required=""></input>
+					<input type="password" name="pswd" placeholder="Password" required=""></input>
+					<button>Sign up</button>
+				</form>
+			</div>
 
-			  <div class="signup">
-				  <form>
-					  <label for="chk" aria-hidden="true">Sign up</label>
-					  <input type="text" name="txt" placeholder="User name" required=""></input>
-					  <input type="email" name="email" placeholder="Email" required=""></input>
-					  <input type="password" name="pswd" placeholder="Password" required=""></input>
-					  <button>Sign up</button>
-				  </form>
-			  </div>
-
-			    <div class="login">
-				    <form>
-					  <label for="chk" aria-hidden="true">Login</label>
-					  <input type="email" name="email" placeholder="Email" required=""></input>
-					  <input type="password" name="pswd" placeholder="Password" required=""></input>
-					  <button>Login</button>
-			  	  </form>
-		    	</div>
-	    </div>
-      </body>
+			<div class="login">
+					<form>
+						<label for="chk" aria-hidden="true">Login</label>
+						<input type="email" name="email" placeholder="Email" required=""></input>
+						<input type="password" name="pswd" placeholder="Password" required=""></input>
+						<button>Login</button>
+					</form>
+				</div>
+			</div>
+		</body>
     );
 }
 
