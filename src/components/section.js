f3595ef30a712b4eae51120673564f8b06444da0
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-15 17:50:51 +0300

    aaa

--- src/components/section.js
+++ src/components/section.js
@@ -5,7 +5,17 @@ function Section() {
     return(
         <section>
             <div className="section">
-                
+                <header>
+                    <h1>ROKET LINK</h1><hr></hr>
+                </header>
+                <div className="content">
+                    <p className="text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                        Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.
+
+                    </p>
+                </div>
             </div>
         </section>
     )
