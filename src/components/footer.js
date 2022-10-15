88d90ed50dc0b80b495ceb2eec67e6ca6cd0ba9f
Alperenkarslix <alperenkarsliceng@gmail.com>
2022-10-15 17:51:52 +0300

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,16 @@ function Footer(){
         <div>
             <footer>
                 <div className="footer">
-                    <p>
-                       2020 © All Rights Reserved. 
-                    </p>
+                <ul>
+                    <li><a href="https://twitter.com/julesforrest">Twitter</a></li>
+                    <li><a href="https://codepen.io/julesforrest">Codepen</a></li>
+                    <li><a href="mailto:julesforrest@gmail.com">Email</a></li>
+                    <li><a href="https://dribbble.com/julesforrest">Dribbble</a></li>
+                    <li><a href="https://github.com/julesforrest">Github</a></li>
+                    <li>
+                    <p>👋</p>
+                    </li>
+                </ul>
                 </div>
 
             </footer>
