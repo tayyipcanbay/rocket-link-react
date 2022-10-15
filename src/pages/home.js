db133ef807cec6813dd0dda644b1e3fa98169369
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-15 16:56:51 +0300

    footer

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,15 +3,9 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
 function Home() {
-    var data= [
-        {
-            name:"Alperen",
-            surname:"Karslı"
-        }
-        ]
     return (
         <div style={{backgroundColor:"black",width:"100vw",height:"100vh"}}>
-            <Navbar data={data} />
+            <Navbar />
             
             
         </div>
