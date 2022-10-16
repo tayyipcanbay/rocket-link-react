5b6da296decd5042a00e6e7700b06f117f299bbc
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-16 17:42:08 +0300

    user sayfası

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Footer from "../components/footer";
 import "../style/user.css";
+import LinkItem from "../components/linkItem";
 
 function User() {
   return (
-    <div>
-        User
+    <div className="user-body">
+      <div className="user-container">
+        <div className="user-header">
+          <div className="user-avatar">
+            <img src="https://picsum.photos/200" alt="user avatar" />
+            <div className="user-name"><pre>@JohnDoe</pre></div><hr></hr>
+            <div className="user-bio"><pre><a>This is Bio title.For the user</a></pre></div>
+          </div>
+        </div>
+        <div className="user-content">
+          <div className="user-info">
+            <LinkItem />
+            <LinkItem />
+            <LinkItem />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
 
   )
