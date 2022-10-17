8958ba4493faa251bf067625560316b3b54c92f3
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-17 15:33:25 +0300

    login eskisi

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,73 +1,31 @@
 import React from "react";
 import '../style/login.css';
-
-
 function Login() {
-	// create a function for checks password length and characters
-	const checkPassword = (e) => {
-		let password=e.target.value;
-		// check password length
-		if (password.length < 8) {
-			return false;
-		}
-		// check password characters
-		let hasNumber = false;
-		let hasUpperCase = false;
-		let hasLowerCase = false;
-		let hasSpecial = false;
-		for (let i = 0; i < password.length; i++) {
-			if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57) {
-				hasNumber = true;
-			} else if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90) {
-				hasUpperCase = true;
-			} else if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122) {
-				hasLowerCase = true;
-			} else {
-				hasSpecial = true;
-			}
-		}
-		if (hasNumber && hasUpperCase && hasLowerCase && hasSpecial) {
-			e.target.style.border = "3px solid green";
-			return true;
-		} else {
-			e.target.style.border = "3px solid red";
-			return false;
-		}
+	const handleLogin=(e)=>{
+		e.preventDefault();
 	}
-	const checkUsername = (e) => {
-		let username = e.target.value;
-		if (username.length < 8) {
-			e.target.style.border = "3px solid red";
-			return false;
-		} else {
-			e.target.style.border = "3px solid green";
-			return true;
-		}
+	const handleRegister=(e)=>{
+		e.preventDefault();
 	}
-	//create a function for check email
-
-
-
   return (
 		<div className="out" >
 			<div className="main">  	
 				<input type="checkbox" id="chk" aria-hidden="true"></input>
 			<div className="signup">
-				<form name="form1">
-					<label htmlFor="chk" aria-hidden="true">Sign up</label>
-					<input type="text" onChange={checkUsername} name="txt" placeholder="Username" required=""></input>
-					<input type="email" name="email" placeholder="Email" required=""></input>
-					<input type="password" onChange={checkPassword} name="pswd" placeholder="Password" required=""></input>
-					<button >Sign up</button>
+				<form onSubmit={handleRegister} name="form1">
+					<label htmlFor="chk" aria-hidden="true">Register</label>
+					<input type="text"  name="username" placeholder="Username" required=""></input>
+					<input type="email"  name="email" placeholder="Email" required=""></input>
+					<input type="password"  name="pswd" placeholder="Password" required=""></input>
+					<button >Register</button>
 				</form>
 			</div>
-
 			<div className="login">
-					<form name="form1">
+					<form onSubmit={handleLogin} name="form1">
 						<label htmlFor="chk" aria-hidden="true">Login</label>
-						<input type="text" onChange={checkUsername} name="username" placeholder="Username" required=""></input>
-						<input type="password" onChange={checkPassword} name="pswd" placeholder="Password" required=""></input>
-						<button >Login</button>
+						<input type="text"  name="username" placeholder="Username" required=""></input>
+						<input type="password"  name="pswd" placeholder="Password" required=""></input>
+						<button onClick={handleLogin} >Login</button>
 					</form>
 				</div>
 			</div>
