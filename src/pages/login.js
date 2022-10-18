338151f5266895b776ffff9791d9f091484b63bb
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-18 18:14:49 +0300

    push

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '../style/login.css';
 function Login() {
 	const [username, setUsername] = React.useState("");
 	const [password, setPassword] = React.useState("");
 	const [email , setEmail] = React.useState("");
+	const inputBorder=(e)=>{
+		if(e.target.name==="username"){
+			if(checkUsername(username)){
+				e.target.style.border="2px solid green";
+			}
+			else{
+				e.target.style.border="2px solid red";
+			}
+		}
+		else if (e.target.name==="password"){
+			if(checkPassword(password)){
+				e.target.style.border="2px solid green";
+			}
+			else{
+				e.target.style.border="2px solid red";
+			}
+		}
+		else if (e.target.name==="email"){
+			if(checkEmail(email)){
+				e.target.style.border="2px solid green";
+			}
+			else{
+				e.target.style.border="2px solid red";
+			}
+		}
+	}
 	const checkUsername=()=>  {
 		if(username.length<5){
-			alert("Username must be more than 5 characters");
 			return false;
 		}
 		return true;
 	}
 	const checkPassword=()=>  {
 		if(password.length<8){
-			alert("Password must be more than 8 characters");
 			return false;
 		}
 		return true;
 	}
 	const checkEmail=()=>  {
 		if(email.length<8){
-			alert("Email must be more than 8 characters");
 			return false;
 		}
 		return true;
@@ -93,17 +116,17 @@ function Login() {
 			<div className="signup">
 				<form onSubmit={handleRegister} name="form1">
 					<label htmlFor="chk" aria-hidden="true">Register</label>
-					<input onChange={(e)=>{setUsername(e.target.value)}} type="text"  name="username" placeholder="Username" required=""></input>
-					<input onChange={(e)=>{setEmail(e.target.value)}} type="email"  name="email" placeholder="Email" required=""></input>
-					<input onChange={(e)=>{setPassword(e.target.value)}} type="password"  name="pswd" placeholder="Password" required=""></input>
+					<input onChange={(e)=>{inputBorder(e);setUsername(e.target.value)}} id="register-username" type="text" name="username" placeholder="Username" required=""></input>
+					<input onChange={(e)=>{inputBorder(e);setEmail(e.target.value)}} id="register-email" type="email"  name="email" placeholder="Email" required=""></input>
+					<input onChange={(e)=>{inputBorder(e);setPassword(e.target.value)}} id="register-password" type="password"  name="pswd" placeholder="Password" required=""></input>
 					<button >Register</button>
 				</form>
 			</div>
 			<div className="login">
 					<form onSubmit={handleLogin} name="form2">
 						<label htmlFor="chk" aria-hidden="true">Login</label>
-						<input onChange={(e)=>{setUsername(e.target.value)}} type="text"  name="username" placeholder="Username" required=""></input>
-						<input onChange={(e)=>{setPassword(e.target.value)}} type="password"  name="pswd" placeholder="Password" required=""></input>
+						<input onChange={(e)=>{inputBorder(e);setUsername(e.target.value)}} id="login-username" type="text"  name="username" placeholder="Username" required=""></input>
+						<input onChange={(e)=>{inputBorder(e);setPassword(e.target.value)}} id="login-password" type="password"  name="pswd" placeholder="Password" required=""></input>
 						<button onClick={handleLogin} >Login</button>
 					</form>
 				</div>
