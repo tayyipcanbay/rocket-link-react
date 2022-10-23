90cd6b8c25cf570df2b5b0c0d6f2373f9a64cfa2
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-23 18:33:55 +0300

    yedek

---
+++ src/components/loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../style/loading.css";
+
+function Loading(){
+    return(
+        <div class="lds-dual-ring"></div>
+    )
+}
+export default Loading;
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,83 +1,65 @@
 import React, { useEffect } from "react";
 import "../style/user.css";
+import Loading from "../components/loading";
 import LinkItem from "../components/linkItem";
 
-function Admin() {
-    const [ready , setReady] = React.useState(false);
-    const [links, setLinks] = React.useState([]);
-    useEffect(()=>{
-        const checkLogin=()=>{
-            var user=JSON.parse(localStorage.getItem("rocket-link-user"));
-            if(user){
-                setReady(true);
-                getUserLinks(user.token).then((data)=>{
-                    if(data.success){
-                        setLinks(data);
-                    }
-                    else{
-                        alert("Error");
-                    }
-                }) 
-                return true;
-            }
-            setTimeout(()=>{
-                window.location.href="/login";
-            },3000)
-            return false;
+function Admin() { 
+    const [user, setUser] = React.useState(JSON.parse(localStorage.getItem("rocket-link-user")));
+    const [isAuth, setIsAuth] = React.useState(false);
+    const [ready, setReady] = React.useState(false);
+    useEffect(() => {
+        if(!user){
+            window.location.href="/login";
         }
-        checkLogin();
-    },[])
-    const getUserLinks=(token)=>{
-            return new Promise((resolve,reject)=>{
-                fetch("http://localhost:9000/api/links/",{
-                method:"GET",
-                headers:{
-                    Authorization:"Bearer "+token
-                }
-            })
-            .then(res=>res.json())
-            .then(data=>{
-                console.log(data);
-                resolve(data);
-            })
-            .catch(err=>{
-                reject(err);
-            })
-            })
-    }
-    const renderPage=()=>{
-        if(ready){
-            return(
-                <div className="user-body">
-                    <div className="user-container">
-                        <div className="user-header">
-                            <div className="user-avatar">
-                                <img src="https://picsum.photos/200" alt="user avatar" />
-                                <div className="user-name"><pre>@JohnDoe</pre></div><hr></hr>
-                                <div className="user-bio"><pre><a>This is Bio title.For the user</a></pre></div>
+        else{
+            setIsAuth(true);
+        }
+    }, []);
+    useEffect(() => {
+        if(isAuth && ready){
+            console.log("admin page");
+            renderPage("loaded");
+            //I want to render the page after the user is authenticated and the page is ready
+        }
+        else if(isAuth && !ready){
+            console.log("Loading your data...");
+            renderPage("loading");
+        }
+    }, [isAuth, ready]);
+    setTimeout(() => {
+        setReady(true);
+    }, 2000);
+    const renderPage=(param)=>{           
+            if(param==="loading"){
+                console.log("loading");
+                return <Loading/>
+            }
+            else if(param==="loaded"){
+                console.log("loaded");  
+                return(
+                    <div className="user-body">
+                        <div className="user-container">
+                            <div className="user-header">
+                                <div className="user-avatar">
+                                    <img src="https://picsum.photos/200" alt="user avatar" />
+                                    <div className="user-name"><pre>@JohnDoe</pre></div><hr></hr>
+                                    <div className="user-bio"><pre><a>This is Bio title.For the user</a></pre></div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="user-content">
-                            <button type="button" onClick={(e)=>{e.preventDefault();console.log(links)}}></button>
-                            <div className="user-info">
-                                <LinkItem />
-                                <LinkItem />
-                                <LinkItem />
+                            <div className="user-content">
+                                <button type="button" onClick={(e)=>{e.preventDefault()}}></button>
+                                <div className="user-info">
+                                    User Info Cards here
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            )
-        }
-        else{
-            return(
-                <div>Please Login First!</div>
-            )
-        }
+                )
+            }
     }
-    return (
-        <div>
-            {renderPage()}
+    return(
+        <div className="admin-body">
+            
         </div>
     )
 }
