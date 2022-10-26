88ef95c553a410f1df212bc9526060b88c6d0407
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-10-26 14:16:48 +0300

    navbar temel

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,14 @@ import {Link }  from "react-router-dom";
 function Navbar(){
     return(
         <nav>
-          
+          <div className="nav-wrapper">
+            <Link to="/" className="brand-logo">ROCKET LİNK</Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><Link to="#">Home</Link></li>
+                <li><Link to="#about">About</Link></li>
+                <li><Link to="#contact">Contact</Link></li>
+            </ul>
+          </div>
         </nav>
 
     )
